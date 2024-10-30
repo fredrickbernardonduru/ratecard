3fcd91994d97f566509527b0821a96cabea2a9a6
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-10-30 17:54:52 +0300

    Fixed the error

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -4,18 +4,28 @@ import Icons from '../global/icons'
 
 const Navbar = () => {
   return (
-    <div>
+    
         <header className='px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50'>
             <div className='flex items-center justify-between h-full mx-auto md:max-w-screen-xl'>
                 <div className='flex items-start'>
-                    <Link href="/">
+                    <Link href="/" className='flex items-center gap-2'>
                     <Icons.logo className='w-8 h-8'/>
+                    <span className='text-lg font-medium'>
+                       Rate Card 
+                    </span>
                         </Link>
                 </div>
+
+                <nav className='hidden md:flex absolute left-1/2 top-1/2
+                -translate-x-1/2 -translate-y-12 transform'>
+                   <ul className="flex items-center justify-center gap-8">
+                    <li className="hover:text-foreground/80 text-sm">
+                    Pricing
+                    </li>
+                   </ul> 
+                </nav>
             </div>
         </header>
-
-    </div>
   )
 }
 
