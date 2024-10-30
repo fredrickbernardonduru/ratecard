003404e015c7adf5fb0241aec4fbc29992a8ef89
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-10-30 17:16:34 +0300

    NavBar update

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        Navbar
+        <header className='px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50'>
+            Navbar
+        </header>
+
     </div>
   )
 }
