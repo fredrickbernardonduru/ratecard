638bffa1a69c8445439eb197da10d763ff368ac5
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-10-30 17:40:20 +0300

    update after adding the logo

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
+import Icons from '../global/icons'
 
 const Navbar = () => {
   return (
     <div>
         <header className='px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50'>
-            Navbar
+            <div className='flex items-center justify-between h-full mx-auto md:max-w-screen-xl'>
+                <div className='flex items-start'>
+                    <Link href="/">
+                    <Icons.logo className='w-8 h-8'/>
+                        </Link>
+                </div>
+            </div>
         </header>
 
     </div>
